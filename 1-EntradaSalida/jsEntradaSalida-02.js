eb2cbeb1fa6e1f42fcb88d72fcb7de3aa2f5f0c4
Adrian Filippelli <adrianfilippelli2@gmail.com>
2023-01-31 12:03:53 -0300

    hice los ejercicios 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,10 +1,10 @@
 /*
+Adrian Filippelli
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombrePersona;  //Declaración de variable
+	let nombrePersona;  
 	nombrePersona = prompt("Cuál es tu nombre");
 	alert(nombrePersona);
 }
-
