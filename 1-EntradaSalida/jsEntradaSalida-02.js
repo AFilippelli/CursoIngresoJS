cbb63c4891e74e0fe025b9705a739a36a8120940
Adrian Filippelli <adrianfilippelli2@gmail.com>
2023-02-01 22:54:18 -0300

    guardar trabajos

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -6,5 +6,9 @@ function mostrar()
 {
 	let nombrePersona;  
 	nombrePersona = prompt("Cuál es tu nombre");
-	alert(nombrePersona);
+	let apellidoPersona;
+	apellidoPersona = prompt("cuál es tu apellido")
+	alert("Usted se llama " + nombrePersona + " Y su apellido es " + apellidoPersona)
 }
+//lo que va a la derecha del = es el origen
+//lo que va a la izquierda del = es destino
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,23 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
-
+	let nombre;
+	nombre = document.getElementById("txtIdNombre").value;
+	alert(nombre);
+	document.getElementById("txtIdNombre").value = "";
+	
 }
+//APUNTES
+/*
+nombre = document.getElementById("txtIdNombre").value
+Lo que se estaría haciendo es copiar los archivos de .html
+para luego ser asignados en la variable "nombre".
+*/
 
-
+/*
+document.getElementById("txtIdNombre").value = "";
+Este comando se utiliza para que, luego de escribir en el buscador
+y que nuestro mensaje salga en la barra emergente, al cerrarla
+el buscador quede vacio y se borre automaticamente el mensaje anterior
+y así poder escribir cualquier otra cosa que queramos.
+*/
